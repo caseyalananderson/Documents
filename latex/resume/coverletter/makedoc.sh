a5dbe727081758c6d1dd61317b3bc269f54e1da2
caseyalananderson <casey.alan.anderson@gmail.com>
2017-11-01 21:44:24 -0600

    added latex repo of my resume

---
+++ latex/resume/coverletter/makedoc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+xe=xelatex
+bi=biber
+
+tex=coverletter_main.tex
+bcf=coverletter_main.bcf
+pdf=coverletter_main.pdf
+
+# comiple resume
+$xe $tex
+$bi $bcf
+$xe $tex
+
+gnome-open $pdf
